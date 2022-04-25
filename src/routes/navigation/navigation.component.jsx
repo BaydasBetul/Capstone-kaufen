e2038fedf64597e8191003d17471805893463178
Bbaydas <bbaydas85@gmail.com>
2022-04-26 00:39:53 +0200

    first

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./navigation.styles.scss";
+import { ReactComponent as Logojpg } from "../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Logojpg className="logo" />
+        </Link>
+        <div className="links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+      {/* Outlet - A component that renders the next match in a set of matches. */}
+    </Fragment>
+  );
+};
+
+export default Navigation;
