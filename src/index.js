afd60c1ca8d14c4fe2a3423461e602cba082268f
Bbaydas <bbaydas85@gmail.com>
2022-12-13 21:37:42 +0100

    redux, logger

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.scss";
 import App from "./App";
@@ -14,9 +15,11 @@ const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
